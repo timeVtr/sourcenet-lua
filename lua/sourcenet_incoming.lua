dda81bbbdabfb1e524971bea20a86632a52c39ed
time <themajorbear1@gmail.com>
2021-06-30 03:01:14 -0500

    initial commit

---
+++ lua/sourcenet_incoming.lua
@@ -0,0 +1,113 @@
+--[[if SERVER then
+	include("server.lua")
+else
+	include("client.lua")
+end]]--
+
+--include("netmessages.lua")
+
+-- Initialization
+
+HookNetChannel(
+	-- nochan prevents a net channel being passed to the attach/detach functions
+	-- CNetChan::ProcessMessages doesn't use a virtual hook, so we don't need to pass the net channel
+	{name = "CNetChan::ProcessMessages", nochan = true}
+)
+
+local function CopyBufferEnd(dst, src)
+	local bitsleft = src:GetNumBitsLeft()
+	local data = src:ReadBits(bitsleft)
+	
+	dst:WriteBits(data)
+end
+
+local specialmsg
+local specialhandler = {
+	DefaultCopy = function(netchan, read, write)
+		specialmsg:ReadFromBuffer(read)
+		specialmsg:WriteToBuffer(write)
+	end
+}
+hook.Add("PreProcessMessages", "InFilter", function(netchan, read, write, localchan)
+	local totalbits = read:GetNumBitsLeft() + read:GetNumBitsRead()
+
+	local islocal = netchan == localchan
+	if not game.IsDedicated() and ((islocal and SERVER) or (not islocal and CLIENT)) then
+		CopyBufferEnd(write, read)
+		return
+	end
+
+	hook.Call("BASE_PreProcessMessages", nil, netchan, read, write)
+
+	local changeLevelState = false
+
+	while read:GetNumBitsLeft() >= NET_MESSAGE_BITS do
+		local msg = read:ReadUInt(NET_MESSAGE_BITS)
+
+		local handler = NET_MESSAGES[msg]
+
+		--[[if msg ~= net_NOP and msg ~= 3 and msg ~= 9 then
+			Msg("(in) Pre Message: " .. msg .. ", bits: " .. read:GetNumBitsRead() .. "/" .. totalbits .. "\n")
+		end--]]
+	
+		if not handler then
+			handler = NET_MESSAGES.CLC[msg]
+
+			if not handler then
+				for i = 1, netchan:GetNetMessageNum() do
+					local m = netchan:GetNetMessage(i)
+					if m:GetType() == msg then
+						handler = specialhandler
+						specialmsg = m
+						break
+					end
+				end
+
+				if not handler then
+					--Msg("Unknown outgoing message: " .. msg .. "\n")
+						
+					write:Seek(totalbits)
+
+					break
+				end
+			end
+		end
+
+		local func = handler.IncomingCopy or handler.DefaultCopy
+
+		local success, ret = xpcall(func, debug.traceback, netchan, read, write)
+		if not success then
+			print(ret)
+
+			break
+		elseif ret == false then
+		--if func(netchan, read, write) == false then
+			Msg("Failed to filter message " .. msg .. "\n")
+
+			write:Seek(totalbits)
+
+			break
+		end
+
+		--[[if msg ~= net_NOP and msg ~= 3 and msg ~= 9 then
+			Msg("(in) Post Message: " .. msg .. " bits: " .. read:GetNumBitsRead() .. "/" .. totalbits .. "\n")
+		end--]]
+	end
+	
+end)
+
+function FilterIncomingMessage(msg, func)
+	local handler = NET_MESSAGES[msg]
+
+	if not handler then
+		handler = NET_MESSAGES.CLC[msg]
+	end
+
+	if handler then
+		handler.IncomingCopy = func
+	end
+end
+
+function UnFilterIncomingMessage(msg)
+	FilterIncomingMessage(msg, nil)
+end
---
+++ lua/sourcenet_netmessages.lua
@@ -0,0 +1,216 @@
+--require("sourcenet")
+
+-- Debug ConVar
+local sourcenet_netmessage_info = CreateConVar("sourcenet_netmessage_info", "0")
+
+local function log2(val)
+	return math.ceil(math.log(val) / math.log(2))
+end
+
+-- Engine definitions
+NET_MESSAGE_BITS = 6
+NUM_NEW_COMMAND_BITS = 4
+NUM_BACKUP_COMMAND_BITS = 3
+MAX_TABLES_BITS = log2(32)
+MAX_USERMESSAGE_BITS = 11
+MAX_ENTITYMESSAGE_BITS = 11
+MAX_SERVER_CLASS_BITS = 9
+MAX_EDICT_BITS = 13
+
+function SourceNetMsg(msg)
+	if sourcenet_netmessage_info:GetInt() ~= 0 then
+		Msg("[snmi] " .. msg)
+	end
+end
+
+-- add limits here for the netmessages we want (just svc_VoiceData for now)
+
+SOURCENET_LIMITS = SOURCENET_LIMITS or {}
+local LIMITS = SOURCENET_LIMITS
+-- svc_VoiceData fix provided for free by time from garnetgaming, enjoy :)
+LIMITS[svc_VoiceData] = {}
+local time = SysTime
+
+NET_MESSAGES = {
+	[net_NOP] = { -- 0
+		DefaultCopy = function(netchan, read, write)
+			write:WriteUInt(net_NOP, NET_MESSAGE_BITS)
+		end
+	},
+
+	[net_Disconnect] = { -- 1
+		DefaultCopy = function(netchan, read, write)
+			write:WriteUInt(net_Disconnect, NET_MESSAGE_BITS)
+
+			local reason = read:ReadString()
+			write:WriteString(reason)
+
+			SourceNetMsg(string.format("net_Disconnect %s\n", reason))
+		end
+	},
+
+	[net_File] = { -- 2
+		DefaultCopy = function(netchan, read, write)
+			write:WriteUInt(net_File, NET_MESSAGE_BITS)
+
+			local transferid = read:ReadUInt(32)
+			write:WriteUInt(transferid, 32)
+
+			local requested = read:ReadBit()
+			write:WriteBit(requested)
+
+			if requested == 0 then
+				SourceNetMsg(string.format("net_File %i,false\n", transferid))
+				return
+			end
+
+			local requesttype = read:ReadUInt(1)
+			write:WriteUInt(requesttype, 1)
+
+			local fileid = read:ReadUInt(32)
+			write:WriteUInt(fileid, 32)
+
+			SourceNetMsg(string.format("net_File %i,true,%i,%i\n", transferid, requesttype, fileid))
+		end
+	},
+
+	[net_Tick] = { -- 3
+		DefaultCopy = function(netchan, read, write)
+			write:WriteUInt(net_Tick, NET_MESSAGE_BITS)
+
+			local tick = read:ReadLong()
+			write:WriteLong(tick)
+
+			local hostframetime = read:ReadUInt(16)
+			write:WriteUInt(hostframetime, 16)
+
+			local hostframetimedeviation = read:ReadUInt(16)
+			write:WriteUInt(hostframetimedeviation, 16)
+
+			--SourceNetMsg(string.format("net_Tick %i,%i,%i\n", tick, hostframetime, hostframetimedeviation))
+		end
+	},
+
+	[net_StringCmd] = { -- 4
+		DefaultCopy = function(netchan, read, write)
+			write:WriteUInt(net_StringCmd, NET_MESSAGE_BITS)
+
+			local cmd = read:ReadString()
+			write:WriteString(cmd)
+
+			SourceNetMsg(string.format("net_StringCmd %s\n", cmd))
+		end
+	},
+
+
+	[net_SignonState] = { -- 6
+		DefaultCopy = function(netchan, read, write)
+			write:WriteUInt(net_SignonState, NET_MESSAGE_BITS)
+
+			local state = read:ReadByte()
+			write:WriteByte(state)
+
+			local servercount = read:ReadLong()
+			write:WriteLong(servercount)
+
+			SourceNetMsg(string.format("net_SignonState %i,%i\n", state, servercount))
+		end
+	},
+
+	CLC = {
+
+		[clc_VoiceData] = { -- 10
+			DefaultCopy = function(netchan, read, write)
+				write:WriteUInt(clc_VoiceData, NET_MESSAGE_BITS)
+
+				local bits = read:ReadWord()
+				write:WriteWord(bits)
+
+				if bits > 0 then
+					local data = read:ReadBits(bits)
+					write:WriteBits(data)
+				end
+
+				SourceNetMsg(string.format("clc_VoiceData %i\n", bits))
+			end
+		},
+	},
+
+	SVC = {
+		[svc_VoiceInit] = { -- 14
+			DefaultCopy = function(netchan, read, write)
+				write:WriteUInt(svc_VoiceInit, NET_MESSAGE_BITS)
+
+				local codec = read:ReadString()
+				write:WriteString(codec)
+
+				local quality = read:ReadByte()
+				write:WriteByte(quality)
+
+				SourceNetMsg(string.format("svc_VoiceInit codec=%s,quality=%i\n", codec, quality))
+			end
+		},
+
+		[svc_VoiceData] = { -- 15
+			DefaultCopy = function(netchan, read, write)
+				write:WriteUInt(svc_VoiceData, NET_MESSAGE_BITS)
+
+				local client = read:ReadByte()
+				write:WriteByte(client)
+
+				local proximity = read:ReadByte()
+				write:WriteByte(proximity)
+
+				local bits = read:ReadWord()
+				write:WriteWord(bits)
+
+				if bits > 0 then -- this is where normal voice messages of > 0 bytes are broadcasted to clients
+					local voicedata = read:ReadBits(bits)
+					write:WriteBits(voicedata)
+				else -- this is where we handle a svc_VoiceData message of 0 bytes
+					-- get player by adding 1 to their client number
+					local ply = ents.GetByIndex(client + 1)
+					
+					-- initialize player's limiter (we don't want to ban based off just one instance)
+					local LIMITS = LIMITS[svc_VoiceData]
+					local limit = LIMITS[ply]
+					local now = time()
+					
+					if not limit then
+						limit = {
+							lasttime = now,
+							firsttime = now,
+							calls = 1,
+							banned = false
+						}
+						LIMITS[ply] = limit
+					-- reset every 2 seconds
+					elseif now > limit.firsttime + 2 then
+						limit.firsttime = now
+						limit.lasttime = now
+						limit.calls = 1
+					-- if limit of 1000 not yet reached, increment limit.calls by 1
+					elseif limit.calls < 1000 then
+						limit.calls = limit.calls + 1
+						limit.lasttime = now		
+					else -- more than 1000 instances of svc_VoiceData 0 in past 2 seconds
+						-- save crasher's steamid
+						local crasher_steamid = ply:SteamID()
+						
+						-- immediately disconnect player from the server
+						ply:Kick("no pls")
+						
+						if not limit.banned then
+							-- ban crasher_steamid here if you wish --
+							--
+							
+							limit.banned = true -- important, or you may end up banning them 5 or 6 times due to the speed at which
+												-- the messages are coming in
+						end
+					end
+				end
+				SourceNetMsg(string.format("svc_VoiceData client=%i,proximity=%i,bits=%i\n", client, proximity, bits))
+			end
+		},
+	}
+}
